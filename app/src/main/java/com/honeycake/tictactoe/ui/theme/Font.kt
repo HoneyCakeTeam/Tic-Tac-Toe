3f538135d566f00344c4ae14ecc14a3475ab3e30
Mennaakram <mennaakram976@gmail.com>
2023-07-11 06:46:26 +0300

    add: font

---
+++ app/src/main/java/com/honeycake/tictactoe/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.honeycake.tictactoe.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.honeycake.tictactoe.R
+
+val IrishGrover = FontFamily(
+    Font(R.font.irish_grover_regular, weight = FontWeight.Normal)
+)
--- app/src/main/java/com/honeycake/tictactoe/ui/theme/Type.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/theme/Type.kt
@@ -6,29 +6,41 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = IrishGrover,
+        fontWeight = FontWeight.Normal,
+        fontSize = 64.sp,
+        color = TextColor
+    ),
+    displayMedium = TextStyle(
+        fontFamily = IrishGrover,
+        fontWeight = FontWeight.Normal,
+        fontSize = 40.sp,
+        color = TextColor
+    ),
+    titleLarge = TextStyle(
+        fontFamily = IrishGrover,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        color = TextColor
+    ),
+    titleMedium = TextStyle(
+        fontFamily = IrishGrover,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        color = TextColor
+    ),
+    labelMedium = TextStyle(
+        fontFamily = IrishGrover,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        color = TextColor
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = IrishGrover,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = TextColor
     )
-    */
 )
