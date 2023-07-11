361d5d9b624e325cb04bed2ed1bd1716be81b8bf
Mennaakram <mennaakram976@gmail.com>
2023-07-11 08:04:46 +0300

    update: component button

--- app/src/main/java/com/honeycake/tictactoe/ui/composable/ButtonItem.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/composable/ButtonItem.kt
@@ -1,4 +1,4 @@
-package com.honeycake.tictactoe.ui.composable.spacing
+package com.honeycake.tictactoe.ui.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -17,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.honeycake.tictactoe.ui.theme.Card
 import com.honeycake.tictactoe.ui.theme.RoundedShape
@@ -52,10 +50,4 @@ fun ButtonItem(
         }
 
     }
-}
-
-@Preview
-@Composable
-fun ButtonITemPrev() {
-    ButtonItem("Create Game")
 }
