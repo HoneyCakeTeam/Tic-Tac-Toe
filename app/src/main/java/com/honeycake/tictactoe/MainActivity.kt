6b31479df3c16c82a13c6aece9f9e27948e37197
Mennaakram <mennaakram976@gmail.com>
2023-07-14 16:29:15 +0300

    update: main activity

--- app/src/main/java/com/honeycake/tictactoe/MainActivity.kt
+++ app/src/main/java/com/honeycake/tictactoe/MainActivity.kt
@@ -3,16 +3,19 @@ package com.honeycake.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.honeycake.tictactoe.ui.screen.create_game.LoadGameScreen
-import com.honeycake.tictactoe.ui.theme.TicTacToeTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.honeycake.tictactoe.ui.TicTacToeApp
+import com.honeycake.tictactoe.ui.theme.Primary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
-            TicTacToeTheme {
-                LoadGameScreen()
-            }
+            val systemUiController = rememberSystemUiController()
+            systemUiController.setStatusBarColor(Primary)
+            TicTacToeApp()
         }
     }
 }
