39493c239cc7bf0d93af8d61589e4e66ab1fdf40
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-16 01:01:02 +0300

    refactor GameScreen

--- app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameScreen.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameScreen.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.honeycake.tictactoe.R
 import com.honeycake.tictactoe.ui.LocalNavigationProvider
 import com.honeycake.tictactoe.ui.composable.IconBack
@@ -20,12 +23,14 @@ import com.honeycake.tictactoe.ui.screen.game.composable.PlayersInfo
  * Created by Aziza Helmy on 7/12/2023.
  */
 @Composable
-fun GameScreen() {
-    GameContent(GameUiState(), onButtonClicked = {})
+fun GameScreen(gameViewModel: GameViewModel = hiltViewModel()) {
+    val gameUiState by gameViewModel.state.collectAsState()
+
+    GameContent(gameUiState, onButtonClicked = gameViewModel::onButtonClick)
 }
 
 @Composable
-private fun GameContent(gameUiState: GameUiState, onButtonClicked: () -> Unit) {
+private fun GameContent(gameUiState: GameUiState, onButtonClicked: (Int) -> Unit) {
     // this will be an event in view model 'onCLickBackButton()'
     val navController = LocalNavigationProvider.current
     Column(
@@ -36,7 +41,7 @@ private fun GameContent(gameUiState: GameUiState, onButtonClicked: () -> Unit) {
         IconBack(
             painter = painterResource(id = R.drawable.arrow_left),
             modifier = Modifier.padding(16.dp)
-        ) {navController.popBackStack()}
+        ) { navController.popBackStack() }
         PlayersInfo(gameUiState)
         GameBoard(gameUiState, onButtonClicked = onButtonClicked)
     }
