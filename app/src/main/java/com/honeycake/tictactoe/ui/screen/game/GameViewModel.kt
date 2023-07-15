f3e9bb40f2ac47c5cf164978a9c4c5c20100f8b2
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-16 00:59:34 +0300

    implement some fun

--- app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameViewModel.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameViewModel.kt
@@ -1,39 +1,78 @@
 package com.honeycake.tictactoe.ui.screen.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class GameViewModel @Inject constructor():ViewModel() {
+class GameViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow(GameUiState())
-    val state =_state.asStateFlow()
+    val state = _state.asStateFlow()
 
 
     fun onButtonClick(buttonIndex: Int) {
-        val currentGameState = _state.value.gameState.toMutableList()
-        val currentPlayer = if (currentGameState.count { it.image == null } % 2 == 0) {
-            _state.value.firstPlayerUiState
+        val currentState = _state.value
+        val currentPlayer = if (currentState.gameState.count { it.image == null } % 2 == 0) {
+            currentState.firstPlayerUiState
         } else {
-            _state.value.secondPlayerUiState
+            currentState.secondPlayerUiState
         }
 
-        if (currentGameState[buttonIndex].enabled) {
-            currentGameState[buttonIndex].enabled = false
-            currentGameState[buttonIndex].image = currentPlayer.playerRole
+        val currentGameState = currentState.gameState.toMutableList()
+        val currentButtonState = currentGameState[buttonIndex]
 
-            _state.value = _state.value.copy(gameState = currentGameState)
+        if (currentButtonState.enabled) {
 
-            if (checkWinCondition(currentPlayer.playerRole)) {
-                //if win end game and show in ui
+            updateButtonState(currentButtonState, currentPlayer.playerRole)
+
+            val updatedState = currentState.copy(gameState = currentGameState)
+            _state.value = updatedState
+
+            if (checkWin(currentPlayer.playerRole)) {
+                val winner = determineWinner(currentState, currentPlayer.playerRole)
+                val updatedWinner = updatePlayerAsWinner(currentState, winner)
+
+                _state.value = updatedState.copy(
+                    firstPlayerUiState = updatedWinner.firstPlayerUiState,
+                    secondPlayerUiState = updatedWinner.secondPlayerUiState
+                )
+                Log.d("GameViewModel", "Player ${winner.playerName} wins!")
             }
         }
 
-}
-    private fun checkWinCondition(playerRole: Int): Boolean {
+    }
+
+    private fun updateButtonState(buttonState: ButtonState, playerRole: Int) {
+        buttonState.enabled = false
+        buttonState.image = playerRole
+    }
+
+    private fun determineWinner(currentState: GameUiState, playerRole: Int): PlayerUiState {
+        return if (playerRole == currentState.firstPlayerUiState.playerRole) {
+            currentState.firstPlayerUiState
+        } else {
+            currentState.secondPlayerUiState
+        }
+    }
+
+    private fun updatePlayerAsWinner(
+        currentState: GameUiState,
+        winner: PlayerUiState
+    ): GameUiState {
+        val updatedFirstPlayer =
+            currentState.firstPlayerUiState.copy(isWinner = winner == currentState.firstPlayerUiState)
+        val updatedSecondPlayer =
+            currentState.secondPlayerUiState.copy(isWinner = winner == currentState.secondPlayerUiState)
+        return currentState.copy(
+            firstPlayerUiState = updatedFirstPlayer,
+            secondPlayerUiState = updatedSecondPlayer
+        )
+    }
+
+    private fun checkWin(playerRole: Int): Boolean {
         val gameState = _state.value.gameState
         val horizontalLines = _state.value.horizontalLines
         val verticalLines = _state.value.verticalLines
