6fd1f6bfdb224efb3fbdf3ed5147be4b01c334a6
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-07-15 22:26:31 +0300

    add logic for game view model

--- app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameUiState.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameUiState.kt
@@ -8,7 +8,21 @@ import com.honeycake.tictactoe.R
 data class GameUiState(
     val firstPlayerUiState: PlayerUiState = PlayerUiState(),
     val secondPlayerUiState: PlayerUiState = PlayerUiState(),
-    val gameState: List<ButtonState> = List(9) { ButtonState(null, true) }
+    val gameState: List<ButtonState> = List(9) { ButtonState(null, true)},
+    val horizontalLines: List<List<Int>> = listOf(
+        listOf(0, 1, 2),
+        listOf(3, 4, 5),
+        listOf(6, 7, 8)
+    ),
+    val verticalLines: List<List<Int>> = listOf(
+        listOf(0, 3, 6),
+        listOf(1, 4, 7),
+        listOf(2, 5, 8)
+    ),
+    val diagonalLines: List<List<Int>> = listOf(
+        listOf(0, 4, 8),
+        listOf(2, 4, 6)
+    )
 )
 
 
--- app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameViewModel.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/game/GameViewModel.kt
@@ -1,7 +1,53 @@
 package com.honeycake.tictactoe.ui.screen.game
 
-/**
- * Created by Aziza Helmy on 7/12/2023.
- */
-class GameViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class GameViewModel @Inject constructor():ViewModel() {
+    private val _state = MutableStateFlow(GameUiState())
+    val state =_state.asStateFlow()
+
+
+    fun onButtonClick(buttonIndex: Int) {
+        val currentGameState = _state.value.gameState.toMutableList()
+        val currentPlayer = if (currentGameState.count { it.image == null } % 2 == 0) {
+            _state.value.firstPlayerUiState
+        } else {
+            _state.value.secondPlayerUiState
+        }
+
+        if (currentGameState[buttonIndex].enabled) {
+            currentGameState[buttonIndex].enabled = false
+            currentGameState[buttonIndex].image = currentPlayer.playerRole
+
+            _state.value = _state.value.copy(gameState = currentGameState)
+
+            if (checkWinCondition(currentPlayer.playerRole)) {
+                //if win end game and show in ui
+            }
+        }
+
 }
+    private fun checkWinCondition(playerRole: Int): Boolean {
+        val gameState = _state.value.gameState
+        val horizontalLines = _state.value.horizontalLines
+        val verticalLines = _state.value.verticalLines
+        val diagonalLines = _state.value.diagonalLines
+
+        for (line in horizontalLines + verticalLines + diagonalLines) {
+            if (line.all { gameState[it].image == playerRole }) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+
+
