5f80eac575165ea08da65674b70f7404e553fe4f
Mennaakram <mennaakram976@gmail.com>
2023-07-18 15:15:01 +0300

    edit:listener

--- app/src/main/java/com/honeycake/tictactoe/ui/screen/join_game/JoinGameInteractionListener.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/join_game/JoinGameInteractionListener.kt
@@ -1,7 +1,5 @@
 package com.honeycake.tictactoe.ui.screen.join_game
 
-import androidx.navigation.NavController
-
 interface JoinGameInteractionListener {
-    fun onJoinGameClicked(navController: NavController)
+    fun onJoinGameClicked()
 }
