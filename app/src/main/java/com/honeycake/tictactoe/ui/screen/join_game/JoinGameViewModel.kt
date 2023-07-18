74d652b566a1c4eb579c6622277aae397631a28c
Mennaakram <mennaakram976@gmail.com>
2023-07-18 15:13:45 +0300

    edit:join view model

--- app/src/main/java/com/honeycake/tictactoe/ui/screen/join_game/JoinGameViewModel.kt
+++ app/src/main/java/com/honeycake/tictactoe/ui/screen/join_game/JoinGameViewModel.kt
@@ -1,45 +1,50 @@
 package com.honeycake.tictactoe.ui.screen.join_game
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
-import com.honeycake.tictactoe.repository.XORepository
+import com.honeycake.tictactoe.data.GameSession
+import com.honeycake.tictactoe.domain.repository.XORepository
 import com.honeycake.tictactoe.ui.base.BaseViewModel
-import com.honeycake.tictactoe.ui.screen.create_game.GameSession
-import com.honeycake.tictactoe.ui.screen.load_game.navigateToLoad
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class JoinGameViewModel @Inject constructor(
     private val XORepository: XORepository,
-): BaseViewModel<JoinGameUiState>(JoinGameUiState()), JoinGameInteractionListener{
+) : BaseViewModel<JoinGameUiState>(JoinGameUiState()), JoinGameInteractionListener {
 
     fun onChangePlayerName(name: String) {
-        updateState { it.copy(secondPlayerName = name)}
+        updateState { it.copy(secondPlayerName = name) }
     }
 
     fun onChangeGameId(id: String) {
         updateState { it.copy(gameId = id, isButtonEnabled = true) }
     }
 
-      override fun onJoinGameClicked(navController: NavController) {
-        if (state.value.secondPlayerName.isNotEmpty() && state.value.gameId.isNotEmpty()) {
-            saveGameSession()
-            navController.navigateToLoad(_state.value.gameId)
-        } else
-      return
-    }
-    
-    private fun saveGameSession() {
-        val gameSession = GameSession(
-            "",
-            _state.value.secondPlayerName,
-            false,
-            _state.value.gameId
-        )
-        viewModelScope.launch {
-            XORepository.saveGameSession(gameSession)
+    override fun onJoinGameClicked() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                if (state.value.secondPlayerName.isNotEmpty() && state.value.gameId.isNotEmpty()) {
+                    updateGameSession()
+                    updateState { it.copy(navigate = true) }
+                }
+            }catch (e: Throwable){
+                Log.e("TAG", "onJoinGameClicked: ${e.message}", )
+            }
         }
+
+    }
+
+    private suspend fun updateGameSession() {
+            XORepository.updateGameSession(
+                GameSession(
+                    _state.value.firstPlayerName,
+                    _state.value.secondPlayerName,
+                    true,
+                    _state.value.gameId
+                )
+            )
     }
 }
